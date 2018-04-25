a36e2a75b63d9dc50199fe7b6bf9c56344a8428c
Oliwia M <okm@aber.ac.uk>
2018-04-25 17:06:39 +0100

    fix for the max number to switch the axis around

--- VideoTracking/DataAnalysing/GraphMaxNumber.py
+++ VideoTracking/DataAnalysing/GraphMaxNumber.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 plt.figure(1)
 
 # plot the maximum number with week number.
-plt.plot([8, 11, 12, 16, 17], [1, 2, 3, 4, 5], 'r^', [8, 11, 12, 16, 17], [1, 2, 3, 4, 5], 'r',
-         [11, 12, 15, 13, 15], [1, 2, 3, 4, 5], 'bs', [11, 12, 15, 13, 15], [1, 2, 3, 4, 5], 'b')
-plt.axis([6, 18, 0, 6])
+plt.plot([1, 2, 3, 4, 5], [8, 11, 12, 16, 17], 'r^', [1, 2, 3, 4, 5], [8, 11, 12, 16, 17], 'r',
+         [1, 2, 3, 4, 5], [11, 12, 15, 13, 15], 'bs', [1, 2, 3, 4, 5], [11, 12, 15, 13, 15], 'b')
+plt.axis([0, 6, 6, 18])
 plt.title("Maximum number of fish around food ring between buzzer and food dropping")
 plt.ylabel("Number of fish")
 plt.xlabel("Weeks")
